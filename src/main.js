4b44687ae1315520a7595385f6d7b95dc9259345
JustRollingDevs <justrollingdevs@protonmail.com>
2023-01-11 19:24:48 +0200

    made prints store refresh-proof

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import router from './router'
 import './assets/base.css'
 
+// persist selected stores (prints) on sessionStorage
+const pinia = createPinia().use(piniaPluginPersistedstate);
+
 createApp(App)
-  .use(createPinia())
+  .use(pinia)
   .use(router)
   .mount('#app')
