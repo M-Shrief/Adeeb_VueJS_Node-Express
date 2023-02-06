da52f47804b2215a70e916b855c324f89ae5661f
JustRollingDevs <justrollingdevs@protonmail.com>
2023-02-06 08:35:39 +0200

    added poemID for printed verses from poemPage

--- src/pages/Poem.vue
+++ src/pages/Poem.vue
@@ -1,84 +1,87 @@
 <template>
   <main>
     <div class="container" dir="rtl">
-      <section id="related-data" v-if="getPoem.poet" >
-        <router-link  :to="'/poet/' + getPoem.poet._id" class="link">
+      <section id="related-data" v-if="getPoem.poet">
+        <router-link :to="'/poet/' + getPoem.poet._id" class="link">
           <ShowCasePoet :details="getPoem.poet" />
         </router-link>
-        
+
         <ShowCasePoems :poems="getPoems">
           <h2>قصائد اخري</h2>
         </ShowCasePoems>
       </section>
-      <ShowCasePoem :verses="getPoem.verses" 
-      @print="(print) => addPrint(print)"/>
+      <ShowCasePoem
+        :verses="getPoem.verses"
+        @print="(print) => addPrint({ Poem: getPoem._id, ...print })"
+      />
     </div>
   </main>
 </template>
 
 <script setup>
-import {  onBeforeMount, computed, watch } from 'vue';
-import {useRoute, useRouter } from 'vue-router';
-// stores
-import { usePoemStore } from "../stores/poems";
-import { usePrintsStore } from "../stores/prints";
-// components
-import ShowCasePoem from "../components/ShowCasePoem.vue";
-import ShowCasePoems from "../components/ShowCasePoems.vue";
-import ShowCasePoet from "../components/ShowCasePoet.vue";
+  import { onBeforeMount, computed, watch } from 'vue';
+  import { useRoute, useRouter } from 'vue-router';
+  // stores
+  import { usePoemStore } from '../stores/poems';
+  import { usePrintsStore } from '../stores/prints';
+  // components
+  import ShowCasePoem from '../components/ShowCasePoem.vue';
+  import ShowCasePoems from '../components/ShowCasePoems.vue';
+  import ShowCasePoet from '../components/ShowCasePoet.vue';
 
-const route = useRoute();
-const router = useRouter();
+  const route = useRoute();
+  const router = useRouter();
 
-const poemStore = usePoemStore();
-const getPoem = computed(() => {
-  return poemStore.getPoem
-})
-const getPoems = computed(() => {
-  return poemStore.getPoems
-})
+  const poemStore = usePoemStore();
+  const getPoem = computed(() => {
+    return poemStore.getPoem;
+  });
+  const getPoems = computed(() => {
+    return poemStore.getPoems;
+  });
 
-// updating poems store
-watch(() => route.params.id, (newPoemId) => {
-  poemStore.fetchPoemAndOtherPoems(route.params.id);
+  // updating poems store
+  watch(
+    () => route.params.id,
+    (newPoemId) => {
+      poemStore.fetchPoemAndOtherPoems(route.params.id);
+    }
+  );
 
-})
+  onBeforeMount(() => {
+    poemStore.fetchPoemAndOtherPoems(route.params.id);
+  });
 
-onBeforeMount(() => {
-  poemStore.fetchPoemAndOtherPoems(route.params.id);
-})
-
-const printsStore = usePrintsStore();
-function addPrint(print) {
-  return printsStore.addPrint(print)
-}
+  const printsStore = usePrintsStore();
+  function addPrint(print) {
+    return printsStore.addPrint(print);
+  }
 </script>
 
 <style lang="scss" scoped>
-@import '../assets/mixins.scss';
+  @import '../assets/mixins.scss';
 
-.container {
-  display: grid;
-  grid-template-columns: 40% 60%;
-  margin: 1rem;
-}
-#related-data {
-  .link {
-  text-decoration: none;
+  .container {
+    display: grid;
+    grid-template-columns: 40% 60%;
+    margin: 1rem;
   }
-  h2 {
-    color: #f6b352;
-    text-align: center;
-    @include mQ($breakpoint-lg) {
-      font-size: 1.1rem;
+  #related-data {
+    .link {
+      text-decoration: none;
     }
-    @include mQ($breakpoint-md) {
-      font-size: 1rem;
-    }
-    @include mQ($breakpoint-sm) {
-      font-size: 1rem;
+    h2 {
+      color: #f6b352;
+      text-align: center;
+      @include mQ($breakpoint-lg) {
+        font-size: 1.1rem;
+      }
+      @include mQ($breakpoint-md) {
+        font-size: 1rem;
+      }
+      @include mQ($breakpoint-sm) {
+        font-size: 1rem;
+      }
     }
   }
-}
-
 </style>
