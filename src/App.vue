f747ef9476d9f1158b8183b08d9c523218896f35
JustRollingDevs <justrollingdevs@protonmail.com>
2023-02-10 07:38:36 +0200

    using v-slot instead of nesting router-view inside transition tag

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,11 @@
   <header>
     <Navbar />
   </header>
-  <Transition name="fade">
-    <RouterView :key="route.path" />
-  </Transition>
+  <router-view v-slot="{ Component }">
+    <transition name="fade" appear>
+      <component :is="Component" />
+    </transition>
+  </router-view>
   <KeepAlive>
     <SelectedPrints dir="rtl" v-if="route.name != 'printing'"/> 
   </KeepAlive>
